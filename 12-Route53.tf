57e285c09c9938126eaf75d023ffaf700cd940d4
jlesproject <143852314+jlesproject@users.noreply.github.com>
2023-11-20 19:41:30 -0400

    Add files via upload

---
+++ 12-Route53.tf
@@ -0,0 +1,17 @@
+data "aws_route53_zone" "main" {
+  name         = "cloudcrowdclan.com"  # The domain name you want to look up
+  private_zone = false
+}
+
+
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "cloudcrowdclan.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.app1_alb.dns_name
+    zone_id                = aws_lb.app1_alb.zone_id
+    evaluate_target_health = true
+  }
+}
