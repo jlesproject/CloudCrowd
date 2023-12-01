36cab1648fb412d0d41a3676d849c7c3fba709fc
jlesproject <143852314+jlesproject@users.noreply.github.com>
2023-12-01 01:20:17 -0400

    Add files via upload

--- 9-LoadBalancer.tf
+++ 9-LoadBalancer.tf
@@ -28,3 +28,30 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.app1_tg.arn
   }
 }
+
+data "aws_acm_certificate" "cert" {
+  domain   = "cloudcrowdclan.com"
+  statuses = ["ISSUED"]
+  most_recent = true
+}
+
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.app1_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"  # or whichever policy suits your requirements
+  certificate_arn   = data.aws_acm_certificate.cert.arn
+
+
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app1_tg.arn
+  }
+}
+
+output "lb_dns_name" {
+  value       = aws_lb.app1_alb.dns_name
+  description = "app1-load-balancer-1286541496.me-south-1.elb.amazonaws.com"
+}
